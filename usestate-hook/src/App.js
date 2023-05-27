a41a09224acd8198487cc9e07fd5369f1e0494e0
MD SALIM <mdsalim.iiitr@gmail.com>
2023-05-27 12:24:39 +0530

    rendering array elements using .map() function

--- usestate-hook/src/App.js
+++ usestate-hook/src/App.js
@@ -1,26 +1,15 @@
-// import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
 
-// let value = 10;
-
 
 function App() {
-  const [value, setvalue] = useState(10);
-  const changeValue = ()=>{
-    if(value<15){
-      setvalue(value+1);
-    }
-    else{
-      setvalue(10);
-    }
-  }
+  const [user, setUser] = useState([1, 2, 3]);
   return (
     <div className="App"> 
-      Home Page {value}
-      <div>
-        <button onClick={()=>setvalue((state)=>state+1)} >Change Value</button>
-      </div>
+      Home Page
+      {user.map((item)=>{
+        return <div>{item}</div>;
+      })}
     
     </div>
 
