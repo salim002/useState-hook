bb3fc1e8629f4386cc5a86dae9f84f03e9c15df8
MD SALIM <mdsalim.iiitr@gmail.com>
2023-05-27 15:11:16 +0530

    Adding new object using useState

--- usestate-hook/src/App.js
+++ usestate-hook/src/App.js
@@ -3,15 +3,18 @@ import {useState} from "react";
 
 
 function App() {
-  const [user, setUser] = useState([1, 2, 3]);
+  // const [user, setUser] = useState([1, 2, 3]);
+  const [user, setUser] = useState({
+    no: 1,
+    name: "Md Salim"
+  })
   return (
     <div className="App"> 
       Home Page
-      {user.map((item, index)=>{
-        return <div key={index}>{item}</div>;
-      })}
+      Home page No: {user.no} <br/>
+      Name : {user.name}
       <div>
-        <button onClick={()=>setUser([...user, 6])} >Change Value</button>
+        <button onClick={()=>setUser({...user, no: 2, name: "codewithviju"})} >Change Value</button>
       </div>
     </div>
 
