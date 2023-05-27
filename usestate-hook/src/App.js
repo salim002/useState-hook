5a32c78f69a8b8195b18b20ab20d0cf1532107c1
MD SALIM <mdsalim.iiitr@gmail.com>
2023-05-27 15:28:58 +0530

    Increasing decreasing counter using buttons

--- usestate-hook/src/App.js
+++ usestate-hook/src/App.js
@@ -3,19 +3,14 @@ import {useState} from "react";
 
 
 function App() {
-  // const [user, setUser] = useState([1, 2, 3]);
-  const [user, setUser] = useState({
-    no: 1,
-    name: "Md Salim"
-  })
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App"> 
-      Home Page
-      Home page No: {user.no} <br/>
-      Name : {user.name}
-      <div>
-        <button onClick={()=>setUser({...user, no: 2, name: "codewithviju"})} >Change Value</button>
-      </div>
+      <div>{counter}</div>
+      <div><button onClick={()=>setCounter(counter+1)}> + </button></div>
+      <div><button onClick={()=>setCounter(counter-1)}> - </button></div>
+      <div><button onClick={()=>setCounter(0)}>Reset</button></div>
+
     </div>
 
   );
