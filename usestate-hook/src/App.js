898d751a4280af69776a534b69a1fd717d6baf26
MD SALIM <mdsalim.iiitr@gmail.com>
2023-05-27 11:14:03 +0530

    changing the value using onClick in the button, defining the function inside button

--- usestate-hook/src/App.js
+++ usestate-hook/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+// let value = 10;
+
 
 function App() {
+  const [value, setvalue] = useState(10);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      Home Page {value}
+      <div>
+        <button onClick={()=>{
+          if(value<15){
+            setvalue(value+1);
+          }
+          else{
+            setvalue(10);
+          }
+          // console.log(value);
+        }}>Change Value</button>
+      </div>
+    
     </div>
+
   );
-}
+} 
 
 export default App;
